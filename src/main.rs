85334536ed8ef8f5168c18a1f96919db92b5d57a
KadinTucker <kadintucker@gmail.com>
2019-12-07 15:31:50 +0100

    Added solution for the first part of day 3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::num;
 
 fn main() {
 
@@ -31,6 +32,28 @@ fn main() {
         }
         i += 1;
     }
+
+    // ==== DAY 3 ====
+
+    let wire1 = generate_wire("wire1.txt");
+    let wire2 = generate_wire("wire2.txt");
+
+    let mut min_distance = -1; //All input values appear to be < 1000 in length
+
+    for s1 in &wire1 {
+        for s2 in &wire2 {
+            let new_dist = match intersection(s1, s2) {
+                Some(v) => v,
+                None => min_distance
+            };
+            //println!("{}", new_dist);
+            if new_dist < min_distance || min_distance < 0 {
+                min_distance = new_dist;
+            }
+        }
+    }
+
+    println!("\tMinimum wire crossing distance: {}", min_distance);
 
     // ==== END ====
     println!("\n");
@@ -95,4 +118,89 @@ fn load_and_run_intcode(noun: i32, verb: i32) -> i32 {
     ints_vec[1] = noun as usize;
     ints_vec[2] = verb as usize;
     run_intcode(ints_vec)
+}
+
+/**
+ * Generates a wire, as a vector of WirePiece structs,
+ * loaded from a file with the given name
+ */
+fn generate_wire(filename: &str) -> Vec<WirePiece> {
+    //File IO
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file"); //Returns a string
+    let commands = contents.split(","); //Returns an iterable
+
+    let mut currX = 0;
+    let mut currY = 0;
+
+    let mut pieces: Vec<WirePiece> = Vec::new();
+
+    for c in commands {
+        let direction = c.as_bytes()[0];
+        let num: i32 = c[1..].parse().expect("Failed to parse string to number");
+        //println!("{}", direction == 85 || direction == 76 || direction == 82 || direction == 68);
+        match direction { 
+            82 => {pieces.push(construct_wire_piece(true, currY, currX, currX + num)); currX += num}, //right
+            76 => {pieces.push(construct_wire_piece(true, currY, currX - num, currX)); currX -= num}, //left
+            85 => {pieces.push(construct_wire_piece(false, currX, currY, currY + num)); currY += num}, //up
+            68 => {pieces.push(construct_wire_piece(false, currX, currY - num, currY)); currY -= num}, //down
+            _ => {}
+        }
+    }
+    pieces
+}
+
+/**
+ * Struct defining a segment of a wire
+ * Start should be always less than finish
+ */
+struct WirePiece {
+    horizontal: bool, //Whether or not the wire is horizontal
+    position: i32, //The static position along which the segment runs
+    start: i32, //The starting position
+    finish: i32 //The finishing position
+}
+
+/**
+ * Constructor for the WirePiece struct
+ */
+fn construct_wire_piece(horizontal: bool, position: i32, start: i32, finish: i32) -> WirePiece {
+    WirePiece {
+        horizontal,
+        position,
+        start,
+        finish
+    }
+}
+
+/**
+ * Determines an intersection between two wire segments
+ * Returns an Option that contains a value if there is an intersection and the Manhattan distance of that intersection from the origin
+ */
+fn intersection(first: &WirePiece, second: &WirePiece) -> Option<i32> {
+    let horizontal = first;
+    let vertical = second;
+    if first.horizontal && !second.horizontal {
+        let horizontal = first;
+        let vertical = second;
+    } else if second.horizontal && !first.horizontal {
+        let horizontal = second;
+        let vertical = first;
+    } else {
+        return None;
+    }
+    //println!("first: ({}, {}, {}, {}); second: ({}, {}, {}, {})", first.horizontal, first.position, first.start, first.finish, second.horizontal, second.position, second.start, second.finish);
+    if horizontal.position > vertical.start && horizontal.position < vertical.finish 
+            && vertical.position > horizontal.start && vertical.position < horizontal.finish {
+        //println!("{}", abs(horizontal.position) + abs(vertical.position));
+        return Some(abs(horizontal.position) + abs(vertical.position));
+    }
+    None
+}
+
+fn abs(x: i32) -> i32{
+    //println!("{}", x);
+    if x < 0 {
+        return -x;
+    }
+    x
 }
